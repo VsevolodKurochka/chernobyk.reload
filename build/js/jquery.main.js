eafa21bd385e3f643db059d3056950bfb9c52602
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-09-27 20:56:12 +0300

    Add jquery.parally.min.js

--- build/js/jquery.main.js
+++ build/js/jquery.main.js
@@ -15,19 +15,12 @@ $(document).ready(function () {
 
 	// Collapse
 
-	$(".collapse__group.active").find(".collapse__group-body").slideDown();
-
-	$('.collapse').on('click', '.collapse__group-header', function () {
-		var collapseInner = $(this).parents('.collapse').find('.collapse__group');
-
-		$(this).parent().toggleClass('active');
-
-		$(this).next().slideToggle('slow');
-
-		collapseInner.not($(this).parent()).removeClass('active');
-
-		collapseInner.children('.collapse__group-body').not($(this).next()).slideUp("slow");
-	});
+	if ($(window).width() > 1025) {
+		$('.parallax-image').parally({
+			speed: 0.2,
+			mode: 'transform'
+		});
+	}
 
 	$('[data-action="modal"]').click(function () {
 		var text = $(this).text();
